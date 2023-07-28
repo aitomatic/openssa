da17a32200cc791b3206ae8b82a0ca9f9835c4a1
Christopher Nguyen <ctn@aitomatic.com>
2023-07-28 04:37:52 -0700

    Refactor third-parties to openssm.integrations

--- openssm/__init__.py
+++ openssm/__init__.py
@@ -1,5 +1,4 @@
 from importlib.metadata import version
-
 __version__ = version('openssm')
 
 from openssm.utils.config import Config, logger
@@ -9,7 +8,7 @@ from openssm.utils.utils import Utils
 from openssm.core.slm.base_slm import BaseSLM
 
 from openssm.core.ssm.base_ssm import BaseSSM
-from openssm.core.ssm.openai_ssm import GPT3CompletionSSM, GPT3ChatCompletionSSM
-from openssm.core.ssm.huggingface_ssm import Falcon7bSSM
-from openssm.core.ssm.llama_index_ssm import LlamaIndexSSM
 
+from openssm.integrations.openai.ssm import GPT3CompletionSSM, GPT3ChatCompletionSSM
+from openssm.integrations.huggingface.ssm import Falcon7bSSM
+from openssm.integrations.llamaindex.ssm import LlamaIndexSSM
