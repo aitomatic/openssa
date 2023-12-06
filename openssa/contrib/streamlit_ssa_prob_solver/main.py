5cbbf71ac5a8234acb3b771929322da586809250
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-12-05 23:34:06 -0800

    update experimental SSA code

--- openssa/contrib/streamlit_ssa_prob_solver/main.py
+++ openssa/contrib/streamlit_ssa_prob_solver/main.py
@@ -1,14 +1,5 @@
 from pathlib import Path
 import sys
 
-import streamlit as st
-
 
 sys.path.insert(1, str(Path(__file__).parent.parent.parent.parent))
-
-
-st.set_page_config(page_title='Problem-Solving SSA',
-                   page_icon=None,
-                   layout='wide',
-                   initial_sidebar_state='auto',
-                   menu_items=None)
--- openssa/contrib/streamlit_ssa_prob_solver/pages/Your_SSA_#2.py
+++ openssa/contrib/streamlit_ssa_prob_solver/pages/Your_SSA_#2.py
@@ -4,7 +4,15 @@
 """SSAProbSolver instance #2."""
 
 
+import streamlit as st
+
 from openssa.contrib import StreamlitSSAProbSolver
 
+
+st.set_page_config(page_title='Problem-Solving SSA',
+                   page_icon=None,
+                   layout='wide',
+                   initial_sidebar_state='auto',
+                   menu_items=None)
 
 StreamlitSSAProbSolver(unique_name='SSA #2')
