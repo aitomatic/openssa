16d7b79d0938b0f1e28f66255ef436cf2ed154ff
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-12-05 21:52:09 -0800

    update experimental SSA code

--- openssa/contrib/streamlit_ssa_prob_solver/main.py
+++ openssa/contrib/streamlit_ssa_prob_solver/main.py
@@ -18,9 +18,9 @@ st.set_page_config(page_title='Problem-Solving SSA',
 
 StreamlitSSAProbSolver(unique_name='PROBLEM-SOLVING SSA',
                        domain='Atomic Layer Deposition (ALD) for Semiconductor',
-                       prob=('I want to estimate the ALD process time for 10 cycles, '
+                       prob=('Estimate the ALD process time for 10 cycles, '
                              'each with Pulse Time = 15 secs, Purge Time = 10 secs and negligible Inert'),
-                       expert_heuristics=('Purge Time must be at least as long as the precursor Pulse Time '
-                                          'to ensure that all excess precursor and reaction byproducts are removed '
+                       expert_heuristics=('Purge Time must be at least as long as Pulse Time in each cycle '
+                                          'to ensure all excess precursor and reaction byproducts are removed '
                                           'from the chamber before the next cycle begins'),
                        doc_src_path='s3://aitomatic-public/KnowledgeBase/Semiconductor/ALD')
