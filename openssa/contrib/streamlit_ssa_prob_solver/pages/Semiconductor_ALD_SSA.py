d4f60cb65c09d02aeda5566c7cf0a81ac6622f00
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-12-05 23:33:24 -0800

    update dev SSA code

--- openssa/contrib/streamlit_ssa_prob_solver/pages/Semiconductor_ALD_SSA.py
+++ openssa/contrib/streamlit_ssa_prob_solver/pages/Semiconductor_ALD_SSA.py
@@ -4,8 +4,16 @@
 """Semiconductor ALD SSAProbSolver."""
 
 
+import streamlit as st
+
 from openssa.contrib import StreamlitSSAProbSolver
 
+
+st.set_page_config(page_title='Problem-Solving SSA',
+                   page_icon=None,
+                   layout='wide',
+                   initial_sidebar_state='auto',
+                   menu_items=None)
 
 StreamlitSSAProbSolver(unique_name='PROBLEM-SOLVING SSA',
                        domain='Atomic Layer Deposition (ALD) for Semiconductor',
