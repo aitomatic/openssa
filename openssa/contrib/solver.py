544a2e6ba62609f1b76e3a813c806e8562840e80
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-12-07 09:15:48 -0800

    add OODA-SSA example & refactor supporting submodules

--- openssa/contrib/solver.py
+++ openssa/contrib/solver.py
@@ -2,19 +2,19 @@ from openssa.core.ooda_rag.ooda_rag import Solver
 from openssa.core.ooda_rag.heuristic import DefaultOODAHeuristic
 from openssa.core.ooda_rag.tools import ReasearchAgentTool
 from openssa.contrib.custom import CustomSSM
+from openssa.utils.llm_config import LLMConfig
+from openssa.utils.aitomatic_llm_config import AitomaticLLMConfig
 
 
 class OodaSSA:
-    def __init__(
-        self,
-        task_heuristics,
-        agent_service_context,
-        llm,
-        rag_llm,
-        embed_model,
-        model: str,
-        highest_priority_heuristic: str = "",
-    ):
+    def __init__(self,
+                 task_heuristics,
+                 highest_priority_heuristic: str = "",
+                 agent_service_context=LLMConfig.get_service_context_llama_2_70b(),
+                 llm=AitomaticLLMConfig.get_aitomatic_llm(),
+                 rag_llm=LLMConfig.get_llm_llama_2_70b(),
+                 embed_model=LLMConfig.get_aito_embeddings(),
+                 model='gpt-4-1106-preview'):
         self.llm = llm
         self.rag_llm = rag_llm
         self.embed_model = embed_model
@@ -27,7 +27,7 @@ class OodaSSA:
             highest_priority_heuristic=highest_priority_heuristic,
         )
 
-    def load(self, folder_path: str) -> None:
+    def activate_resources(self, folder_path: str) -> None:
         agent = CustomSSM(llm=self.rag_llm, embed_model=self.embed_model)
 
         if folder_path.startswith('s3://'):
