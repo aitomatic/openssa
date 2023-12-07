2315dfd9fe4f585fa4c3ca0bb51f8ba3b9a7c091
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2023-12-06 22:15:42 -0800

    update experimental SSA code

--- openssa/utils/aitomatic_llm_config.py
+++ openssa/utils/aitomatic_llm_config.py
@@ -9,16 +9,16 @@ class AitomaticLLMConfig:
     @classmethod
     def get_llama2_70b(cls):
         return OpenAI(
-            api_key=os.environ.get("LEPTON_API_KEY"),
+            api_key=os.environ.get("LEPTON_API_KEY", "twoun3dz0fzw289dgyp2rlb3kltti8zi"),
             base_url="https://llama2-70b.lepton.run/api/v1",
         )
 
     @classmethod
     def get_intel_neural_chat_7b(cls):
-        url_base = os.environ.get("AITOMATIC_INTEL_NEURAL_CHAT_7B_URL_BASE", "http://34.145.174.152:8000/v1")
-        llm = OpenAI(
-            base_url=url_base
+        url_base = os.environ.get(
+            "AITOMATIC_INTEL_NEURAL_CHAT_7B_URL_BASE", "http://34.145.174.152:8000/v1"
         )
+        llm = OpenAI(base_url=url_base)
         return llm
 
     @classmethod
@@ -34,4 +34,11 @@ class AitomaticLLMConfig:
         return OpenAI(
             api_key=os.environ.get("OPENAI_API_KEY"),
             base_url="https://api.openai.com/v1",
+        )
+
+    @classmethod
+    def get_aitomatic_llm(cls):
+        return OpenAI(
+            api_key=os.environ.get("AITOMATIC_API_KEY", "AITOMATIC"),
+            base_url="https://aimo-api-mvp.platform.aitomatic.com/api/v1",
         )
