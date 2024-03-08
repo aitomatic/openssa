0779487667499a02ac037e14bf2311c98434933d
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-03-08 14:36:57 -0800

    refactor openssa.l2.resource sub-module

--- openssa/l2/resource/abstract.py
+++ openssa/l2/resource/abstract.py
@@ -13,18 +13,18 @@ class AbstractResource(ABC):
     @cached_property
     @abstractmethod
     def unique_name(self) -> str:
-        """Return globally-unique name."""
+        """Return globally-unique name of informational resource."""
 
     @cached_property
     @abstractmethod
     def name(self) -> str:
-        """Return potentially non-unique, but informationally helpful name."""
+        """Return potentially non-unique, but informationally helpful name of informational resource."""
 
     @abstractmethod
-    def answer(self, question: str) -> str:
+    def answer(self, question: str, n_words: int = 300) -> str:
         """Answer question from informational resource."""
 
     @cached_property
-    def overview(self):
+    def overview(self) -> str:
         """Return overview of informational resource."""
         return self.answer(question=RESOURCE_OVERVIEW_PROMPT_TEMPLATE.format(name=self.name))
