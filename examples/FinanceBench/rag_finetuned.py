1d4dcbc023f8f8975f01c980f56ea1a18339a2da
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-03-14 08:57:37 -0700

    update examples/FinanceBench with fine-tune RAG

--- examples/FinanceBench/rag_finetuned.py
+++ examples/FinanceBench/rag_finetuned.py
@@ -1,5 +1,8 @@
 from argparse import ArgumentParser
 from functools import cache
+import os
+
+from llama_index.embeddings.openai import OpenAIEmbedding
 
 from openssa.l2.resource.file import FileResource
 
@@ -7,9 +10,14 @@ from openssa.l2.resource.file import FileResource
 from data import FbId, DOC_NAMES_BY_FB_ID, QS_BY_FB_ID, cache_dir_path, enable_batch_qa, update_or_create_output_file
 
 
+EMBED_MODEL = OpenAIEmbedding(model='text-embedding-3-large',
+                              api_key=os.environ['AITO_KEY'],
+                              api_base=f'http://{os.environ['AITO_HOST']}:8000/v1_aito')
+
+
 @cache
 def get_or_create_file_resource(doc_name: str) -> FileResource | None:
-    return (FileResource(path=dir_path)
+    return (FileResource(path=dir_path, embed_model=EMBED_MODEL)
             if (dir_path := cache_dir_path(doc_name))
             else None)
 
