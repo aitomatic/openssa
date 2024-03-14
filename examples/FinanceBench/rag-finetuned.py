a20cedaf4af7f6821c0903377927f27b26ae632f
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-03-14 01:18:25 -0700

    add examples/FinanceBench/rag-finetuned

---
+++ examples/FinanceBench/rag-finetuned.py
@@ -0,0 +1,29 @@
+from argparse import ArgumentParser
+from functools import cache
+
+from openssa.l2.resource.file import FileResource
+
+# pylint: disable=wrong-import-order
+from data import FbId, DOC_NAMES_BY_FB_ID, QS_BY_FB_ID, cache_dir_path, enable_batch_qa, update_or_create_output_file
+
+
+@cache
+def get_or_create_file_resource(doc_name: str) -> FileResource | None:
+    return (FileResource(path=dir_path)
+            if (dir_path := cache_dir_path(doc_name))
+            else None)
+
+
+@enable_batch_qa
+@update_or_create_output_file('RAG-FineTuned')
+def answer(fb_id: FbId) -> str:
+    return (file_resource.answer(QS_BY_FB_ID[fb_id])
+            if (file_resource := get_or_create_file_resource(DOC_NAMES_BY_FB_ID[fb_id]))
+            else 'ERROR: doc not found')
+
+
+if __name__ == '__main__':
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument('fb_id')
+    args = arg_parser.parse_args()
+    print(answer(args.fb_id))
