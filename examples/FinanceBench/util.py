7e85c07aa63a276d0602d7d4f467a1081830d552
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-04-01 21:14:45 -0700

    escape curly brackets while logging out answers in examples/FinanceBench/util.log_qa_and_update_output_file(...) decorator

--- examples/FinanceBench/util.py
+++ examples/FinanceBench/util.py
@@ -35,7 +35,8 @@ class log_qa_and_update_output_file:  # noqa: N801
         @wraps(wrapped=qa_func)
         def decorated_qa_func(fb_id: FbId) -> Answer:
             logger.info(f'\n{fb_id}\n{DOC_NAMES_BY_FB_ID[fb_id]}:\n{QS_BY_FB_ID[fb_id]}\n'
-                        f'\n{self.output_name.upper()}:\n{(answer := qa_func(fb_id))}\n',
+                        f'\n{self.output_name.upper()}:\n'
+                        f'{(answer := qa_func(fb_id)).replace('{', '{{').replace('}', '}}')}\n',
                         depth=1)
 
             if OUTPUT_FILE_PATH.is_file():
