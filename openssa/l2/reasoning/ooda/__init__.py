d137c6137935a2e158859ae6b9147b8bc69d5f6f
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-04-20 22:51:16 -0700

    update openssa.l2.reasoning.ooda.OodaReasoner

--- openssa/l2/reasoning/ooda/__init__.py
+++ openssa/l2/reasoning/ooda/__init__.py
@@ -2,11 +2,20 @@
 
 
 from dataclasses import dataclass
+from typing import TypedDict
 
 from openssa.l2.reasoning.abstract import AbstractReasoner
 from openssa.l2.task.abstract import ATask
+from openssa.l2.task.status import TaskStatus
 
-from ._prompts import OBSERVE_PROMPT_TEMPLATE, ORIENT_PROMPT_TEMPLATE, DECIDE_PROMPT_TEMPLATE
+from ._prompts import OBSERVATION_CONSO_PROMPT_TEMPLATE, DECIDE_PROMPT_TEMPLATE
+
+
+type Observation = tuple[str, str, str]
+
+class OrientResult(TypedDict):
+    confident: bool
+    answer: str | None
 
 
 @dataclass
@@ -15,17 +24,47 @@ class OodaReasoner(AbstractReasoner):
 
     def reason(self, task: ATask, n_words: int = 300) -> str:
         """Reason through task and return conclusion."""
+        observations: list[Observation] = self.observe(task=task, n_words=n_words)
+        orient_result: OrientResult = self.orient(task=task, observations=observations, n_words=n_words)
+        decision: bool = self.decide(orient_result=orient_result)
+        self.act(task=task, orient_result=orient_result, decision=decision)
+        return task.result
 
-        return result
+    def observe(self, task: ATask, n_words: int = 300) -> list[Observation]:
+        """Observe answers from informational resources."""
+        return [(r.name, r.overview, r.answer(question=task.ask, n_words=n_words))
+                for r in task.resources]
 
-    def observe(self, task: ATask) -> str:
-        """Observe."""
+    def orient(self, task: ATask, observations: list[Observation], n_words: int = 300) -> OrientResult:
+        """Orient whether observed answers are adequate for direct task resolution."""
+        prompt: str = OBSERVATION_CONSO_PROMPT_TEMPLATE.format(
+            question=task.ask, n_words=n_words,
+            resources_and_answers='\n\n'.join((f'INFORMATIONAL RESOURCE #{i + 1} (name: "{name}"):\n'
+                                               '\n'
+                                               f'INFORMATIONAL RESOURCE #{i + 1} OVERVIEW:\n{overview}\n'
+                                               '\n'
+                                               f'ANSWER #{i + 1}:\n{answer}\n')
+                                              for i, (name, overview, answer) in enumerate(observations)))
 
-    def orient(self, task: ATask) -> str:
-        """Orient."""
+        def is_valid(orient_result_dict: OrientResult) -> bool:
+            return (isinstance(orient_result_dict.get('confident'), bool) and
+                    (('answer' in orient_result_dict) and
+                     (((answer := orient_result_dict['answer']) is None) or
+                      isinstance(answer, str))))
 
-    def decide(self, task: ATask) -> str:
-        """Decide."""
+        # TODO: more rigorous JSON schema validation
+        orient_result_dict: OrientResult = {}
+        while not is_valid(orient_result_dict):
+            orient_result_dict: OrientResult = self.lm.parse_output(self.lm.get_response(prompt))
 
-    def act(self, task: ATask) -> str:
-        """Act."""
+        return orient_result_dict
+
+    def decide(self, orient_result: OrientResult) -> bool:
+        """Decide whether to directly resolve the task."""
+        return orient_result['confident']
+
+    def act(self, task: ATask, orient_result: OrientResult, decision: bool) -> str:
+        """Update task status and result."""
+        if decision:
+            task.status: TaskStatus = TaskStatus.DONE
+            task.result: str = orient_result['answer']
--- openssa/l2/reasoning/ooda/_prompts.py
+++ openssa/l2/reasoning/ooda/_prompts.py
@@ -1,10 +1,27 @@
-OBSERVE_PROMPT_TEMPLATE: str = \
-"""
-"""  # noqa: E122
+OBSERVATION_CONSO_PROMPT_TEMPLATE: str = \
+"""Assuming that the following question/task is asked
+
+```
+{question}
+```
+
+and you have received various answers from different informational resources as detailed below,
+please evaluate whether you can answer the posed question/task confidently.
+
+If you can answer confidently, return a JSON dictionary
+`{{"confident": true, "answer": "<fill in your answer using up to {n_words:,} words>"}}`.
+
+If you cannot answer confidently, return a JSON dictionary
+`{{"confident: false, "answer": null}}`.
+
+Please return ONLY the JSON DICTIONARY and no other text, not even the "```json" wrapping!
+
+Use no other information than the below answers.
 
 
-ORIENT_PROMPT_TEMPLATE: str = \
-"""
+```
+{resources_and_answers}
+```
 """  # noqa: E122
 
 
