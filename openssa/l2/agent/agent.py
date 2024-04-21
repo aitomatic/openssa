8b87bf133db2b945d8236a4ec5c9f5463ff79eec
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-04-21 16:40:30 -0700

    set openssa.l2.agent.agent.Agent.reasoner to default to OodaReasoner

--- openssa/l2/agent/agent.py
+++ openssa/l2/agent/agent.py
@@ -5,6 +5,8 @@ from dataclasses import dataclass, field
 
 from openssa.l2.planning.abstract import APlanner
 from openssa.l2.planning.hierarchical import AutoHTPlanner
+from openssa.l2.reasoning.abstract import AReasoner
+from openssa.l2.reasoning.ooda import OodaReasoner
 
 from .abstract import AbstractAgent
 
@@ -14,3 +16,4 @@ class Agent(AbstractAgent):
     """Agent with planning, reasoning & informational resources."""
 
     planner: APlanner = field(default_factory=AutoHTPlanner)
+    reasoner: AReasoner = field(default_factory=OodaReasoner)
