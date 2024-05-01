acb2f155bd572767c5c00c53bd8328886ad9a4c5
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-05-01 00:53:23 -0500

    update examples/FinanceBench/eval.py

--- examples/FinanceBench/eval.py
+++ examples/FinanceBench/eval.py
@@ -1,109 +1,106 @@
-import os
 import argparse
-import pandas as pd
-import yaml
-from dotenv import load_dotenv, find_dotenv
 
-from openai import OpenAI
+from collections import Counter
+from dotenv import load_dotenv
+from loguru import logger
+import pandas as pd
+from tqdm import tqdm
+
 from openssa.utils.llms import OpenAILLM
 
-def read_csv_file(file_path):
-    try:
-        # Read the CSV file into a DataFrame
-        df = pd.read_csv(file_path)
-        print("CSV file successfully read:")
-        print(df.head())
-        return df
-    except FileNotFoundError:
-        print(f"Error: File not found at {file_path}")
-        return None
-    except Exception as e:
-        print(f"Error occurred while reading CSV file: {e}")
-        return None
-    
-def read_yaml_file(yaml_file_path):
-    try:
-        with open(yaml_file_path, 'r') as file:
-            data = yaml.safe_load(file)
-        return data
-    except FileNotFoundError:
-        print(f"Error: File not found at {yaml_file_path}")
-        return None
-    except Exception as e:
-        print(f"Error occurred while reading YAML file: {e}")
-        return None
+# pylint: disable=wrong-import-order
+from data import FB_ID_COL_NAME, GROUND_TRUTHS, OUTPUT_FILE_PATH
+
+
+EVAL_PROMPT_TEMPLATE = \
+"""
+I need you to act as an objective and precise judge of the correctness of an answer to the QUESTION posed below.
+
+Evaluate whether the ANSWER below is correct according to the grading criteria described in the EVALUATION RUBRIC.
+
+Output only a single word. Say YES if you judge the answer to be correct, and NO if incorrect.
+
+QUESTION:
+```
+{question}
+```
+
+ANSWER:
+```
+{answer}
+```
+
+EVALUATION RUBRIC:
+```
+{rubric}
+```
+"""  # noqa: E122
+
+
+load_dotenv()
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description="Process YAML and CSV file paths.")
-    parser.add_argument("--yaml_file_path", type=str, default="/home/shruti/AITOMATIC/source_code/other_scripts/auto_eval_script/ground-truths.yml",
-                        help="Path to the YAML file (default: %(default)s)")
-    parser.add_argument("--csv_file_path", type=str, default="/home/shruti/AITOMATIC/source_code/other_scripts/auto_eval_script/output.csv",
-                        help="Path to the CSV file (default: %(default)s)")
-    parser.add_argument("--ID_col", type=str, default="financebench_id",
-                        help="Name of the column containing IDs (default: %(default)s)")
-    parser.add_argument("--answer_col", type=str, default="OODA",
-                        help="Name of the column containing answers (default: %(default)s)")
-    parser.add_argument("--financebench_id", type=str, default="financebench_id_00222",
-                        help="Financebench ID (default: %(default)s)")
-    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("answer_col", type=str, help="Name of the column containing answers")
+    parser.add_argument("--id", type=str, help="FinanceBench Question ID", default="all")
     return parser.parse_args()
 
-def get_eval_prompt(financebench_id, reference_data, generated_data, ID_col, answer_col):
-    
-    question = reference_data[financebench_id]['question']
-    rubric = reference_data[financebench_id]['correctness']
-    answer = generated_data[generated_data[ID_col] == financebench_id][answer_col].item()
-    
-    prompt = f"You are an objective and precise assistant. \
-              Evaluate whether the answer generated is correct according to the grading criterion described in the RUBRIC. \
-              Output only a single word. Say YES if you judge the answer to be correct, and NO if incorrect.\
-              question: {question}\
-              answer: {answer} \
-              rubric: {rubric}"
-    return prompt
-    
 
-def create_LLM(model = "gpt-4-1106-preview"):
-    
-    _ = load_dotenv(find_dotenv())
-    OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
-
-    llm = OpenAILLM(model=model, api_key=OPENAI_API_KEY)
-    return llm
-
-def evaluate_question(financebench_id, reference_data, generated_data, ID_col, answer_col): 
-    
-    Prompt = get_eval_prompt(financebench_id, reference_data, generated_data, ID_col, answer_col)
-    
-    llm = create_LLM()
-
-    response = llm.get_response(prompt=Prompt)
-    return response
+def get_eval_prompt(financebench_id, output_df, answer_col):
+    return EVAL_PROMPT_TEMPLATE.format(question=GROUND_TRUTHS[financebench_id]['question'],
+                                       answer=output_df.loc[financebench_id, answer_col],
+                                       rubric=GROUND_TRUTHS[financebench_id]['correctness'])
 
 
-def main(): 
+def get_llm(model="gpt-4-1106-preview"):
+    return OpenAILLM(model=model)
+
+
+def evaluate_question(financebench_id, output_df, answer_col):
+    prompt = get_eval_prompt(financebench_id, output_df, answer_col)
+    llm = get_llm()
+    return llm.get_response(prompt=prompt)
+
+
+def main():
     args = parse_arguments()
-
-    yaml_file_path = args.yaml_file_path
-    csv_file_path = args.csv_file_path
-    ID_col = args.ID_col
     answer_col = args.answer_col
-    financebench_id = args.financebench_id
+    financebench_id = args.id
 
-    reference_data = read_yaml_file(yaml_file_path)
-    generated_data = read_csv_file(csv_file_path)
+    output_df = pd.read_csv(OUTPUT_FILE_PATH, index_col=FB_ID_COL_NAME)
 
-    if financebench_id == "all": 
-        for id in generated_data[ID_col]:
-            score = evaluate_question(id, reference_data, generated_data, ID_col, answer_col)
-            #store score in csv
-            print(score)
+    if financebench_id == "all":
+        n_yes_scores_by_category = {}
 
-    else: 
-        score = evaluate_question(financebench_id, reference_data, generated_data, ID_col, answer_col)
+        for fbid in tqdm(output_df.index):
+            score = evaluate_question(fbid, output_df, answer_col)
+
+            if score == 'YES':
+                category = GROUND_TRUTHS[fbid]['category']
+                if category in n_yes_scores_by_category:
+                    n_yes_scores_by_category[category] += 1
+                else:
+                    n_yes_scores_by_category[category] = 1
+
+            else:
+                logger.warning(
+                    f'QUESTION:\n{GROUND_TRUTHS[fbid]['question']}\n'
+                    '\n'
+                    f'ANSWER JUDGED TO BE WRONG:\n{output_df.loc[fbid, answer_col]}\n'
+                    '\n'
+                    f'RUBRIC:\n{GROUND_TRUTHS[fbid]['correctness']}')
+
+        n_questions_by_category = Counter(_['category'] for _ in GROUND_TRUTHS.values())
+        yes_proportions_by_category = {c: n_yes_scores_by_category[c] / n_questions_by_category[c]
+                                       for c in n_questions_by_category}
+        logger.info(yes_proportions_by_category)
+
+    else:
+        score = evaluate_question(financebench_id, output_df, answer_col)
         print(score)
-        
+
 
 if __name__ == "__main__":
     main()
+
