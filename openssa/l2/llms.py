38207b3cb4b64f762ce5aa43f3f04a8907fb95db
Zooey Nguyen <zooeyn@gmail.com>
2024-05-03 16:24:48 -0700

    style: Make llms class file more efficient

--- openssa/l2/llms.py
+++ openssa/l2/llms.py
@@ -1,52 +1,48 @@
 from __future__ import annotations
-
-from typing import Dict, List
+from dataclasses import dataclass, field
+from abc import ABC, abstractmethod
 
 from llamaapi import LlamaAPI
 from openai import OpenAI
-
 from openssa.l2.config import Config
 
-class AnLLM:
+@dataclass
+class AnLLM(ABC):
     """
     This class provides a consistent API for the different LLM services.
     """
-    def __init__(
-        self,
-        model: str,
-        api_key: str,
-        api_base: str,
-    ):
-        self.model = model
-        self.api_key = api_key
-        self.api_base = api_base
+    model: str
+    api_key: str
+    api_base: str
 
     @classmethod
+    @abstractmethod
     def from_defaults(cls):
         """Get an LLM instance with default parameters."""
-        pass
 
-    def call(self, messages: List[Dict], **kwargs):
+    @abstractmethod
+    def call(self, messages: list, **kwargs):
         """Call the LLM API and return the response object."""
-        pass
 
+    @abstractmethod
+    def get_response(self, prompt: str, messages: list = None, **kwargs):
+        """Call the LLM API and return the response content."""
 
+@dataclass
 class LlamaLLM(AnLLM):
     """Use the Llama client."""
-    def __init__(
-        self,
-        model: str = Config.DEFAULT_LLAMA_MODEL,
-        api_key: str = Config.LLAMA_API_KEY,
-        api_base: str = Config.LLAMA_API_URL,
-    ):
-        super().__init__(model, api_key, api_base)
+    client: LlamaAPI = field(init=False)
+
+    def __post_init__(self):
+        """Initialize the Llama client."""
         self.client = LlamaAPI(api_token=self.api_key, hostname=self.api_base)
 
     @classmethod
-    def from_defaults(cls):
-        return cls()
+    def from_defaults(cls) -> LlamaLLM:
+        """Get an LlamaLLM instance with default parameters."""
+        return cls(model=Config.DEFAULT_LLAMA_MODEL, api_key=Config.LLAMA_API_KEY, api_base=Config.LLAMA_API_URL)
 
-    def call(self, messages: List[Dict], **kwargs):
+    def call(self, messages: list, **kwargs):
         return self.client.run({
             "model": self.model,
             "messages": messages,
@@ -59,22 +55,22 @@ class LlamaLLM(AnLLM):
         messages.append({"role": "user", "content": prompt})
         return self.call(messages, **kwargs)["choices"][0]["message"]["content"]
 
+@dataclass
 class OpenAILLM(AnLLM):
     """Use the OpenAI client."""
-    def __init__(
-        self,
-        model: str = Config.DEFAULT_OPENAI_MODEL,
-        api_key: str = Config.OPENAI_API_KEY,
-        api_base: str = Config.OPENAI_API_URL,
-    ):
-        super().__init__(model, api_key, api_base)
+
+    client: OpenAI = field(init=False)
+
+    def __post_init__(self):
+        """Initialize the OpenAI client."""
         self.client = OpenAI(api_key=self.api_key, base_url=self.api_base)
 
     @classmethod
     def from_defaults(cls):
-        return cls()
+        """Get an OpenAILLM instance with default parameters."""
+        return cls(model=Config.DEFAULT_OPENAI_MODEL, api_key=Config.OPENAI_API_KEY, api_base=Config.OPENAI_API_URL)
 
-    def call(self, messages: List[Dict], **kwargs):
+    def call(self, messages: list, **kwargs):
         return self.client.chat.completions.create(
             model=self.model,
             messages=messages,
