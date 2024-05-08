1d58a451677fed5343f971e5f3a426c9ec30c949
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-05-08 10:07:54 -0700

    Revert "enhance examples/FinanceBench/util"
    This reverts commit 6ca2d97ecaae43839c09a1bc2010958caab2fee6.

--- examples/FinanceBench/util.py
+++ examples/FinanceBench/util.py
@@ -7,7 +7,6 @@ from pandas import DataFrame, read_csv
 from tqdm import tqdm
 
 from data import FbId, Answer, FB_ID_COL_NAME, META_DF, FB_IDS, DOC_NAMES_BY_FB_ID, QS_BY_FB_ID, OUTPUT_FILE_PATH
-from eval import eval_correctness
 
 
 type QAFunc = Callable[[FbId], Answer]
@@ -31,9 +30,6 @@ def enable_batch_qa(qa_func: QAFunc) -> QAFunc:
 class log_qa_and_eval_correctness_and_update_output_file:  # noqa: N801
     output_name: str
 
-    def __post_init__(self):
-        self.correctness_eval_col_name: str = f'{self.output_name}: CORRECT?'
-
     def __call__(self, qa_func: QAFunc) -> QAFunc:
         @wraps(wrapped=qa_func)
         def decorated_qa_func(fb_id: FbId) -> Answer:
@@ -47,11 +43,9 @@ class log_qa_and_eval_correctness_and_update_output_file:  # noqa: N801
 
             else:
                 output_df: DataFrame = META_DF[['doc_name', 'question', 'evidence_text', 'page_number', 'answer']]
-                output_df.loc[:, self.output_name]: str | None = None
-                output_df.loc[:, self.correctness_eval_col_name]: bool | None = None
+                output_df.loc[:, self.output_name] = None
 
-            output_df.loc[fb_id, self.output_name]: str = answer
-            output_df.loc[fb_id, self.correctness_eval_col_name]: bool = eval_correctness(fb_id=fb_id, answer=answer)
+            output_df.loc[fb_id, self.output_name] = answer
 
             output_df.to_csv(OUTPUT_FILE_PATH, index=True)
 
