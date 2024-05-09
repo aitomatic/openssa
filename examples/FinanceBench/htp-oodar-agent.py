fea340b33f7c47411dc7ec6d58e1fd00c5a0efaa
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-05-08 17:33:44 -0700

    update examples/FinanceBench/htp-oodar-agent and related Makefile targets

--- examples/FinanceBench/htp-oodar-agent.py
+++ examples/FinanceBench/htp-oodar-agent.py
@@ -8,7 +8,7 @@ from openssa import Agent, HTP, AutoHTPlanner, OodaReasoner, FileResource
 from data_and_knowledge import (DocName, FbId, Answer,
                                 FB_ID_COL_NAME, DOC_NAMES_BY_FB_ID, QS_BY_FB_ID, EXPERT_PLANS_MAP, EXPERT_PLANS,
                                 cache_dir_path)
-from util import enable_batch_qa_and_eval, log_qa_and_update_output_file
+from util import QAFunc, enable_batch_qa_and_eval, log_qa_and_update_output_file
 
 
 @cache
@@ -20,35 +20,63 @@ def get_or_create_agent(doc_name: DocName) -> Agent | None:
             else None)
 
 
-@enable_batch_qa_and_eval(output_name='HTP-auto-dynamic---OODAR')
-@log_qa_and_update_output_file(output_name='HTP-auto-dynamic---OODAR')
-def solve_automatically_dynamically(fb_id: FbId) -> Answer:
-    return (agent.solve(QS_BY_FB_ID[fb_id])
-            if (agent := get_or_create_agent(DOC_NAMES_BY_FB_ID[fb_id]))
-            else 'ERROR: doc not found')
-
-
 @enable_batch_qa_and_eval(output_name='HTP-auto-static---OODAR')
 @log_qa_and_update_output_file(output_name='HTP-auto-static---OODAR')
-def solve_automatically_statically(fb_id: FbId) -> Answer:
-    plan_dict = EXPERT_PLANS[EXPERT_PLANS_MAP[fb_id]]
-    plan = HTP.from_dict(plan_dict)
-    return (agent.solve(problem=QS_BY_FB_ID[fb_id],
-                        plan=plan,
-                        dynamic=False)
+def solve_auto_htp_statically(fb_id: FbId) -> Answer:
+    return (agent.solve(problem=QS_BY_FB_ID[fb_id], plan=None, dynamic=False)
             if (agent := get_or_create_agent(DOC_NAMES_BY_FB_ID[fb_id]))
             else 'ERROR: doc not found')
+
+
+@enable_batch_qa_and_eval(output_name='HTP-auto-dynamic---OODAR')
+@log_qa_and_update_output_file(output_name='HTP-auto-dynamic---OODAR')
+def solve_auto_htp_dynamically(fb_id: FbId) -> Answer:
+    return (agent.solve(problem=QS_BY_FB_ID[fb_id], plan=None, dynamic=True)
+            if (agent := get_or_create_agent(DOC_NAMES_BY_FB_ID[fb_id]))
+            else 'ERROR: doc not found')
+
+
+@enable_batch_qa_and_eval(output_name='HTP-expert-static---OODAR')
+@log_qa_and_update_output_file(output_name='HTP-expert-static---OODAR')
+def solve_expert_htp_statically(fb_id: FbId) -> Answer:
+    return ((agent.solve(problem=QS_BY_FB_ID[fb_id],
+                         plan=HTP.from_dict(EXPERT_PLANS[expert_htp_id]),
+                         dynamic=False)
+
+             if (expert_htp_id := EXPERT_PLANS_MAP.get(fb_id))
+
+             else agent.solve(problem=QS_BY_FB_ID[fb_id], plan=None, dynamic=True))
+
+            if (agent := get_or_create_agent(DOC_NAMES_BY_FB_ID[fb_id]))
+
+            else 'ERROR: doc not found')
+
+
+@enable_batch_qa_and_eval(output_name='HTP-expert-dynamic---OODAR')
+@log_qa_and_update_output_file(output_name='HTP-expert-dynamic---OODAR')
+def solve_expert_htp_dynamically(fb_id: FbId) -> Answer:  # noqa: ARG001
+    raise NotImplementedError('Dynamic execution of given Plan and Planner not yet implemented')
 
 
 if __name__ == '__main__':
     arg_parser = ArgumentParser()
     arg_parser.add_argument('fb_id')
-    arg_parser.add_argument('--static', action='store_true')
+    arg_parser.add_argument('--expert-plan', action='store_true')
+    arg_parser.add_argument('--dynamic-exec', action='store_true')
     args = arg_parser.parse_args()
 
-    solve = (solve_automatically_statically
-             if args.static
-             else solve_automatically_dynamically)
+    match (args.expert_plan, args.dynamic_exec):
+        case (False, False):
+            solve: QAFunc = solve_auto_htp_statically
+
+        case (False, True):
+            solve: QAFunc = solve_auto_htp_dynamically
+
+        case (True, False):
+            solve: QAFunc = solve_expert_htp_statically
+
+        case (True, True):
+            solve: QAFunc = solve_expert_htp_dynamically
 
     solve(fb_id
           if (fb_id := args.fb_id).startswith(FB_ID_COL_NAME)
