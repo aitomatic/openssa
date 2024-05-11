b1036bfe928ffa273ad60baff0f3b795977db718
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-05-11 11:54:26 -0700

    refactor openssa.l2.reasoning.base.BaseReasoner.reason(...) with use of optional openssa.l2.knowledge.abstract.Knowledge

--- openssa/l2/reasoning/base/__init__.py
+++ openssa/l2/reasoning/base/__init__.py
@@ -2,14 +2,12 @@
 
 
 from dataclasses import dataclass
-from typing import TYPE_CHECKING
 
 from openssa.l2.reasoning.abstract import AbstractReasoner
+from openssa.l2.knowledge.abstract import Knowledge
+from openssa.l2.knowledge._prompts import knowledge_injection_lm_chat_msgs
 from openssa.l2.task.abstract import ATask
 from openssa.l2.task.status import TaskStatus
-
-if TYPE_CHECKING:
-    from openssa.l2.util.lm.abstract import LMChatHist
 
 from ._prompts import RESOURCE_QA_CONSO_PROMPT_TEMPLATE
 
@@ -18,21 +16,20 @@ from ._prompts import RESOURCE_QA_CONSO_PROMPT_TEMPLATE
 class BaseReasoner(AbstractReasoner):
     """Base Reasoner."""
 
-    def reason(self, task: ATask, n_words: int = 1000, knowledge: set[str] = None) -> str:
+    def reason(self, task: ATask, *, knowledge: set[Knowledge] | None = None, n_words: int = 1000) -> str:
         """Work through Task and return conclusion.
 
         Simply forward the question/problem to Task's available Information Resources,
         and then consolidate results from them.
         """
-        messages: LMChatHist = []
-        if knowledge is not None:
-            messages.append({"role": "system", "content": "\n".join(s for s in knowledge)})
         task.result: str = ((self.lm.get_response(
                                 prompt=RESOURCE_QA_CONSO_PROMPT_TEMPLATE.format(
                                     question=task.ask, n_words=n_words,
                                     resources_and_answers='\n\n'.join(r.present_full_answer(question=task.ask, n_words=n_words)  # noqa: E501
                                                                       for r in task.resources)),
-                                history=messages)
+                                history=(knowledge_injection_lm_chat_msgs(knowledge=knowledge)
+                                         if knowledge
+                                         else None))
 
                              if len(task.resources) > 1
 
@@ -40,7 +37,10 @@ class BaseReasoner(AbstractReasoner):
 
                             if task.resources
 
-                            else self.lm.get_response(prompt=f'`[WITHIN {n_words:,} WORDS:]`\n{task.ask}', history=messages))
+                            else self.lm.get_response(prompt=f'`[WITHIN {n_words:,} WORDS:]`\n{task.ask}',
+                                                      history=(knowledge_injection_lm_chat_msgs(knowledge=knowledge)
+                                                               if knowledge
+                                                               else None)))
 
         task.status: TaskStatus = TaskStatus.DONE
 
