ab27ac89b977a4d4ef0bed6ce0b027ef4fa5413c
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-05-14 16:41:03 -0700

    revert changes in examples/FinanceBench/util module

--- examples/FinanceBench/util.py
+++ examples/FinanceBench/util.py
@@ -6,7 +6,7 @@ from loguru import logger
 from pandas import DataFrame, read_csv
 from tqdm import tqdm
 
-from data_and_knowledge import FbId, Answer, FB_ID_COL_NAME, FILTERED_META_DF, FILTERERED_FB_IDS, FILTERERED_DOC_NAMES_BY_FB_ID, FILTERERED_QS_BY_FB_ID, OUTPUT_FILE_PATH  # noqa: E501
+from data_and_knowledge import FbId, Answer, FB_ID_COL_NAME, META_DF, FB_IDS, DOC_NAMES_BY_FB_ID, QS_BY_FB_ID, OUTPUT_FILE_PATH  # noqa: E501
 from eval import eval_correctness, eval_all
 
 
@@ -21,7 +21,7 @@ class enable_batch_qa_and_eval:  # noqa: N801
         @wraps(wrapped=qa_func)
         def decorated_qa_func(fb_id: FbId) -> Answer | None:
             if 'all' in fb_id.lower():
-                for _fb_id in tqdm(FILTERERED_FB_IDS):
+                for _fb_id in tqdm(FB_IDS):
                     qa_func(_fb_id)
 
                 eval_all(output_name=self.output_name)
@@ -40,8 +40,7 @@ class log_qa_and_update_output_file:  # noqa: N801
     def __call__(self, qa_func: QAFunc) -> QAFunc:
         @wraps(wrapped=qa_func)
         def decorated_qa_func(fb_id: FbId) -> Answer:
-            logger.info(
-                        f'\n{fb_id}\n{FILTERERED_DOC_NAMES_BY_FB_ID[fb_id]}:\n{FILTERERED_QS_BY_FB_ID[fb_id]}\n'
+            logger.info(f'\n{fb_id}\n{DOC_NAMES_BY_FB_ID[fb_id]}:\n{QS_BY_FB_ID[fb_id]}\n'
                         f'\n{self.output_name.upper()}:\n'
                         f'{(answer := qa_func(fb_id)).replace('{', '{{').replace('}', '}}')}\n',
                         depth=1)
@@ -50,7 +49,7 @@ class log_qa_and_update_output_file:  # noqa: N801
                 output_df: DataFrame = read_csv(OUTPUT_FILE_PATH, index_col=FB_ID_COL_NAME)
 
             else:
-                output_df: DataFrame = FILTERED_META_DF[['doc_name', 'question', 'evidence_text', 'page_number', 'answer']]
+                output_df: DataFrame = META_DF[['doc_name', 'question', 'evidence_text', 'page_number', 'answer']]
                 output_df.loc[:, self.output_name]: str | None = None
 
             output_df.loc[fb_id, self.output_name]: str = answer
