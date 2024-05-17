a06a4f748c2f1efa06d6ca2a68069cf0835ba8e5
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-05-17 00:19:57 -0700

    enhance openssa.l2.reasoning.base.BaseReasoner.reason(...) to incorporate optional other_results

--- openssa/l2/reasoning/base/__init__.py
+++ openssa/l2/reasoning/base/__init__.py
@@ -10,7 +10,8 @@ from openssa.l2.reasoning.abstract import AbstractReasoner
 from openssa.l2.knowledge._prompts import knowledge_injection_lm_chat_msgs
 from openssa.l2.task.status import TaskStatus
 
-from ._prompts import RESOURCE_QA_CONSO_PROMPT_TEMPLATE
+from ._prompts import (RESOURCE_QA_CONSO_PROMPT_TEMPLATE, RESOURCE_QA_AND_OTHER_RESULTS_CONSO_PROMPT_TEMPLATE,
+                       OTHER_RESULTS_CONSO_PROMPT_TEMPLATE, format_other_result)
 
 if TYPE_CHECKING:
     from openssa.l2.planning.abstract.plan import AskAnsPair
@@ -22,31 +23,57 @@ if TYPE_CHECKING:
 class BaseReasoner(AbstractReasoner):
     """Base Reasoner."""
 
-    def reason(self, task: ATask, *, knowledge: set[Knowledge], other_results: list[AskAnsPair] | None = None, n_words: int = 1000) -> str:
+    def reason(self, task: ATask, *,
+               knowledge: set[Knowledge], other_results: list[AskAnsPair] | None = None, n_words: int = 1000) -> str:
         """Work through Task and return conclusion.
 
         Simply forward the question/problem to Task's available Information Resources,
         and then consolidate results from them.
         """
-        task.result: str = ((self.lm.get_response(
-                                prompt=RESOURCE_QA_CONSO_PROMPT_TEMPLATE.format(
-                                    question=task.ask, n_words=n_words,
-                                    resources_and_answers='\n\n'.join(r.present_full_answer(question=task.ask,
-                                                                                            n_words=n_words ,
-                                                                                            other_results=other_results) # noqa: E501
-                                                                      for r in task.resources)),
-                                history=knowledge_injection_lm_chat_msgs(knowledge=knowledge) if knowledge else None)
+        if task.resources:
+            if len(task.resources) > 1:
+                resources_and_answers_str: str = '\n\n'.join(r.present_full_answer(question=task.ask, n_words=n_words)
+                                                             for r in task.resources)
 
-                             if len(task.resources) > 1
+                task.result: str = self.lm.get_response(
+                    prompt=(RESOURCE_QA_AND_OTHER_RESULTS_CONSO_PROMPT_TEMPLATE.format(
+                                question=task.ask, n_words=n_words,
+                                resources_and_answers=resources_and_answers_str,
+                                other_results='\n\n'.join(format_other_result(other_result)
+                                                          for other_result in other_results))
 
-                             else next(iter(task.resources)).answer(question=task.ask, n_words=n_words))
+                            if other_results
 
-                            if task.resources
+                            else RESOURCE_QA_CONSO_PROMPT_TEMPLATE.format(
+                                question=task.ask, n_words=n_words,
+                                resources_and_answers=resources_and_answers_str)),
 
-                            else self.lm.get_response(prompt=f'`[WITHIN {n_words:,} WORDS:]`\n{task.ask}',
-                                                      history=(knowledge_injection_lm_chat_msgs(knowledge=knowledge)
-                                                               if knowledge
-                                                               else None)))
+                    history=knowledge_injection_lm_chat_msgs(knowledge=knowledge) if knowledge else None)
+
+            elif other_results:
+                task.result: str = self.lm.get_response(
+                    prompt=RESOURCE_QA_AND_OTHER_RESULTS_CONSO_PROMPT_TEMPLATE.format(
+                        question=task.ask, n_words=n_words,
+                        resources_and_answers=next(iter(task.resources)).present_full_answer(question=task.ask,
+                                                                                             n_words=n_words),
+                        other_results='\n\n'.join(format_other_result(other_result) for other_result in other_results)),
+                    history=knowledge_injection_lm_chat_msgs(knowledge=knowledge) if knowledge else None)
+
+            else:
+                task.result: str = next(iter(task.resources)).answer(question=task.ask, n_words=n_words)
+
+        elif other_results:
+            task.result: str = self.lm.get_response(
+                prompt=OTHER_RESULTS_CONSO_PROMPT_TEMPLATE.format(
+                    question=task.ask, n_words=n_words,
+                    other_results='\n\n'.join(format_other_result(other_result) for other_result in other_results)),
+                history=knowledge_injection_lm_chat_msgs(knowledge=knowledge) if knowledge else None)
+
+        else:
+            task.result: str = self.lm.get_response(prompt=f'`[WITHIN {n_words:,} WORDS:]`\n{task.ask}',
+                                                    history=(knowledge_injection_lm_chat_msgs(knowledge=knowledge)
+                                                             if knowledge
+                                                             else None))
 
         task.status: TaskStatus = TaskStatus.DONE
 
