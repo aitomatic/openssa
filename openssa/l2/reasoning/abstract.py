dc6e0b38982289354b080b8746c9625151d79ef1
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-05-16 23:24:54 -0700

    refactor openssa.l2.reasoning.abstract.AbstractReasoner.reason(...) method signature

--- openssa/l2/reasoning/abstract.py
+++ openssa/l2/reasoning/abstract.py
@@ -10,10 +10,10 @@ from typing import TypeVar, TYPE_CHECKING
 from openssa.l2.util.lm.openai import OpenAILM
 
 if TYPE_CHECKING:
+    from openssa.l2.planning.abstract.plan import AskAnsPair
     from openssa.l2.knowledge.abstract import Knowledge
     from openssa.l2.task.abstract import ATask
     from openssa.l2.util.lm.abstract import AnLM
-    from openssa.l2.planning.abstract.plan import AskAnsPair
 
 
 @dataclass
@@ -30,7 +30,8 @@ class AbstractReasoner(ABC):
                      kw_only=False)
 
     @abstractmethod
-    def reason(self, task: ATask, *, knowledge: set[Knowledge], other_results: list[AskAnsPair] | None = None, n_words: int = 1000) -> str:
+    def reason(self, task: ATask, *,
+               knowledge: set[Knowledge], other_results: list[AskAnsPair] | None = None, n_words: int = 1000) -> str:
         """Work through Task and return conclusion."""
 
 
