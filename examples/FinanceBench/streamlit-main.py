08cb4003db599579b31eeab721326d4483307620
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-06-06 00:37:53 -0700

    update examples/FinanceBench/streamlit-main app

--- examples/FinanceBench/streamlit-main.py
+++ examples/FinanceBench/streamlit-main.py
@@ -3,15 +3,14 @@
 
 from loguru import logger
 import streamlit as st
-from pathlib import Path
 
 from streamlit_extras.capture import logcapture
 
 from data_and_knowledge import (DocName, FbId,
                                 DOC_LINKS_BY_NAME, DOC_NAMES_BY_FB_ID, FB_IDS_BY_DOC_NAME, QS_BY_FB_ID,
-                                EXPERT_PLAN_MAP, LOCAL_CACHE_DIR_PATH)
+                                EXPERT_PLAN_MAP)
 from htp_oodar_agent import get_or_create_agent, expert_plan_from_fb_id
-from llama_index.core import StorageContext, load_index_from_storage  # noqa: E402
+from rag_default import get_or_create_file_resource
 
 
 DOC_NAMES: list[DocName] = sorted({DOC_NAMES_BY_FB_ID[fb_id] for fb_id in EXPERT_PLAN_MAP})
@@ -21,19 +20,6 @@ REPRESENTATIVE_FB_IDS_BY_DOC_NAME: dict[FbId, list[DocName]] = {doc_name: (set(F
 
 
 TITLE: str = 'OpenSSA: Analysing SEC Filings with Planning & Reasoning'
-
-
-def get_or_create_simple_rag_ssa(problem_id: str):  # noqa: E999, ANN201
-    DATA_SRC_INDEX_DIR_PATH: Path = LOCAL_CACHE_DIR_PATH / 'docs' / DOC_NAMES_BY_FB_ID[problem_id] / '.text-embedding-ada-002'  # noqa: N806
-    storage_context = StorageContext.from_defaults(persist_dir=DATA_SRC_INDEX_DIR_PATH)
-    index = load_index_from_storage(storage_context)
-    return index.as_query_engine()
-
-
-def get_answer_simple(problem: str, problem_id: str) -> str:
-    ssa = get_or_create_simple_rag_ssa(problem_id)
-    solution = ssa.query(problem).response
-    return solution
 
 
 st.set_page_config(page_title=TITLE,
@@ -76,25 +62,30 @@ problem_id: str = st.selectbox(label='Problem',
                                label_visibility='collapsed')
 
 
-# Initialize session state variables
 if 'question_answered' not in st.session_state:
-    st.session_state.question_answered = False
-if 'current_answer' not in st.session_state:
-    st.session_state.current_answer = ""
-if 'all_answers' not in st.session_state:
-    st.session_state.all_answers = []
+    st.session_state.question_answered: bool = False
 
-if st.button(label='ANSWER WITH TYPICAL RAG'):
-    with st.spinner(text='_SOLVING..._'):
-        solution: str = get_answer_simple(problem=QS_BY_FB_ID[problem_id], problem_id=problem_id)
-        st.session_state.current_answer = solution
-        st.session_state.question_answered = True
+if 'current_answer' not in st.session_state:
+    st.session_state.current_answer: str = ''
+
+
+if st.button(label=f'__ANSWER WITH 1-PASS RAG__: _{QS_BY_FB_ID[problem_id]}_',
+             # key=None,
+             on_click=None, args=None, kwargs=None,
+             type='secondary',
+             disabled=False,
+             use_container_width=False):
+    with st.spinner(text='_ATTEMPTING TO RETRIEVE ANSWER..._'):
+        solution: str = get_or_create_file_resource(doc_name=st.session_state.doc_name).answer(question=QS_BY_FB_ID[problem_id])
+        st.session_state.current_answer: str = solution
+        st.session_state.question_answered: bool = True
 
 if st.session_state.question_answered:
-    st.write(st.session_state.current_answer)
+    st.markdown(body=st.session_state.current_answer)
 
-if st.button(label='ANSWER WITH PLANNING AND REASONING',
-             key=None,
+
+if st.button(label=f'__SOLVE WITH PLANNING & REASONING__: _{QS_BY_FB_ID[problem_id]}_',
+             # key=None,
              on_click=None, args=None, kwargs=None,
              type='primary',
              disabled=False,
@@ -105,7 +96,7 @@ if st.button(label='ANSWER WITH PLANNING AND REASONING',
                          .solve(problem=QS_BY_FB_ID[problem_id], plan=expert_plan_from_fb_id(problem_id), dynamic=False))
 
     if st.session_state.question_answered:
-        st.session_state.all_answers.append(st.session_state.current_answer)
-        st.session_state.question_answered = False
-        st.session_state.current_answer = ""
+        st.session_state.question_answered: bool = False
+        st.session_state.current_answer: str = ''
+
     st.latex(body=solution)
