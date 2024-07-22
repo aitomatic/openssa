6e02e312bc4ba12df76d829937959a3c49907275
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-07-22 09:25:05 -0700

    refactor openssa.neurosym.program.abstract.program_finder.AbstractProgramFinder

--- openssa/neurosym/program/abstract/program_finder.py
+++ openssa/neurosym/program/abstract/program_finder.py
@@ -6,14 +6,14 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from openssa.l2.util.lm.abstract import AnLM
-    from openssa.neurosym.algo.abstract.algo import AbstractAlgo
-    from openssa.neurosym.algo.abstract.algo_lib import AbstractAlgoLib
+    from openssa.neurosym.program.abstract.program import AbstractProgram
+    from openssa.neurosym.program.abstract.program_space import AbstractProgramSpace
 
 
 @dataclass
-class AbstractAlgoFinder(ABC):
+class AbstractProgramFinder(ABC):
     lm: AnLM
 
     @abstractmethod
-    def find_algo(self, problem: str, algo_lib: AbstractAlgoLib | None = None) -> AbstractAlgo:
+    def find_algo(self, problem: str, program_space: AbstractProgramSpace | None = None) -> AbstractProgram:
         """Find algorithm for problem."""
