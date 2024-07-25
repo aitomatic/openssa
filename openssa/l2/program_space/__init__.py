5accfb098f3c74c4c32c58cd8e86fe36f4cca819
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-07-25 11:40:27 -0700

    update openssa.l2.program_space.ProgramSpace

--- openssa/l2/program_space/__init__.py
+++ openssa/l2/program_space/__init__.py
@@ -1,14 +1,26 @@
 from __future__ import annotations
 
+from dataclasses import dataclass, field
 from typing import TYPE_CHECKING
 
+from openssa.l2.util.lm.openai import OpenAILM
+
 if TYPE_CHECKING:
-    from openssa.neurosym.program.abstract import AbstractProgram
+    from openssa.l2.programming.abstract.program import AProgram
+    from openssa.l2.util.lm.abstract import AnLM
 
 
+@dataclass
 class ProgramSpace:
-    def add_program(self, name: str, desc: str, program: AbstractProgram):
-        """Add program to library with unique identifying name & informative description."""
+    descriptions: dict[str, str] = field(default_factory=dict)
+    programs: dict[str, AProgram] = field(default_factory=dict)
 
-    def find_program(self, problem: str) -> AbstractProgram:
+    lm: AnLM = field(default_factory=OpenAILM)
+
+    def add_or_update_program(self, name: str, description: str, program: AProgram):
+        """Add program to library with unique identifying name & informative description."""
+        self.descriptions[name]: str = description
+        self.programs[name]: AProgram = program
+
+    def find_program(self, problem: str) -> AProgram:
         """Find program for problem."""
