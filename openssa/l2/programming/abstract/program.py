384ade2d588dc54fc5ba1f8710a61ee84215526b
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-07-29 11:14:21 -0700

    refactor openssa.l2.programming.abstract.program submodule

--- openssa/l2/programming/abstract/program.py
+++ openssa/l2/programming/abstract/program.py
@@ -3,33 +3,26 @@
 ABSTRACT PROGRAM INTERFACE
 ==========================
 
-`AbstractProgram` is `OpenSSA`'s abstract base class for problem-solving programs.
+`AbstractProgram` is `OpenSSA`'s abstract base class for problem-solving Programs.
 
-A program has a target `task` to solve, and can contain decomposed `sub_programs` for solving that `task`.
+A Program has a target Task,
+which encapsulates a posed Problem to solve and a set of Resources to help solve it.
 
-A program can be executed through its own `.execute(...)` method,
-by a specified Reasoner that optionally takes into account some given domain-specific Knowledge and/or other results.
-The program execution returns a string result.
+A Program can be executed through its own `.execute(...)` method,
+which optionally takes into account some given domain-specific Knowledge,
+and which returns a string result.
 """
 
 
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from dataclasses import dataclass, field
-from pprint import pformat
-from types import SimpleNamespace
-from typing import Any, Self, TypeVar, TYPE_CHECKING
+from dataclasses import dataclass
+from typing import TypeVar, TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from openssa.l2.reasoning.abstract import AReasoner
     from openssa.l2.knowledge.abstract import Knowledge
     from openssa.l2.task import Task
-    from openssa.l2.util.misc import AskAnsPair
-
-
-class PLAN(SimpleNamespace):
-    pass  # namespace class just for pretty-printing
 
 
 @dataclass
@@ -39,48 +32,11 @@ class AbstractProgram(ABC):
     # target Task to solve
     task: Task
 
-    # decomposed Sub-Plans for solving target Task
-    sub_programs: list[Self] = field(default_factory=list,
-                                     init=True,
-                                     repr=True,
-                                     hash=None,
-                                     compare=True,
-                                     metadata=None,
-                                     kw_only=False)
-
-    def concretize_tasks_from_template(self, **kwargs: Any):
-        self.task.ask: str = self.task.ask.format(**kwargs)
-
-        for sub_program in self.sub_programs:
-            sub_program.concretize_tasks_from_template(**kwargs)
-
-    @property
-    def quick_repr(self) -> PLAN:
-        """Quick, pretty-formattable/printable namespace representation."""
-        namespace: PLAN = PLAN(task=self.task.ask)
-
-        if self.sub_programs:
-            namespace.subs: list[PLAN] = [sub_program.quick_repr for sub_program in self.sub_programs]
-
-        return namespace
-
-    @property
-    def pformat(self) -> str:
-        """Pretty-formatted string representation."""
-        return pformat(object=self.quick_repr,
-                       indent=2,
-                       width=120,
-                       depth=None,
-                       compact=False,
-                       sort_dicts=False,
-                       underscore_numbers=False).replace("'", '').replace('\\n', '')
-
     @abstractmethod
-    def execute(self, reasoner: AReasoner, knowledge: set[Knowledge] | None = None,
-                other_results: list[AskAnsPair] | None = None) -> str:
-        """Execute and return string result, using specified Reasoner to work through involved Task & Sub-Tasks.
+    def execute(self, knowledge: set[Knowledge] | None = None, **kwargs) -> str:
+        """Execute and return string result.
 
-        Execution also optionally takes into account domain-specific Knowledge and/or potentially elevant other results.
+        Execution also optionally takes into account domain-specific Knowledge.
         """
 
 
