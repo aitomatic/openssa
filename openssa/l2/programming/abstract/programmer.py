3b450214277c134c8cfea5b427f58ddc7df2d9e2
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-07-29 11:34:20 -0700

    update openssa.l2.programming.abstract.programmer submodule

--- openssa/l2/programming/abstract/programmer.py
+++ openssa/l2/programming/abstract/programmer.py
@@ -3,10 +3,7 @@
 ABSTRACT PROGRAMMER INTERFACE
 =============================
 
-`AbstractProgrammer` is `OpenSSA`'s abstract base class for using LMs to create or update problem-solving task plans.
-
-A programmer has an LM for generating new or updated task plans,
-the complexity of which is controlled by at least 2 key parameters `max_depth` and `max_subtasks_per_decomp`.
+`AbstractProgrammer` is `OpenSSA`'s abstract base class for using LMs to construct problem-solving Programs.
 """
 
 
@@ -14,22 +11,22 @@ from __future__ import annotations
 
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
-from typing import Self, TypeVar, TYPE_CHECKING
+from typing import TypeVar, TYPE_CHECKING
 
 from openssa.l2.util.lm.openai import OpenAILM
 
 if TYPE_CHECKING:
-    from openssa.l2.resource.abstract import AResource
     from openssa.l2.knowledge.abstract import Knowledge
+    from openssa.l2.resource.abstract import AResource
     from openssa.l2.util.lm.abstract import AnLM
-    from .plan import APlan
+    from .program import AProgram
 
 
 @dataclass
 class AbstractProgrammer(ABC):
     """Abstract Programmer."""
 
-    # language model for generating solution Plans
+    # language model for generating solution Programs
     lm: AnLM = field(default_factory=OpenAILM.from_defaults,
                      init=True,
                      repr=True,
@@ -38,26 +35,11 @@ class AbstractProgrammer(ABC):
                      metadata=None,
                      kw_only=False)
 
-    # generally applicable parameters for controlling generated Plans' allowed complexity
-    max_depth: int = 2
-    max_subtasks_per_decomp: int = 3
-
-    def one_level_deep(self) -> Self:
-        """Get 1-level-deep Planner with same other parameters."""
-        return type(self)(lm=self.lm, max_depth=1, max_subtasks_per_decomp=self.max_subtasks_per_decomp)
-
-    def one_fewer_level_deep(self) -> Self:
-        """Get 1-fewer-level-deep Planner with same other parameters."""
-        return type(self)(lm=self.lm, max_depth=self.max_depth - 1, max_subtasks_per_decomp=self.max_subtasks_per_decomp)  # noqa: E501
-
     @abstractmethod
-    def plan(self, problem: str, *, knowledge: set[Knowledge] | None = None, resources: set[AResource] | None = None) -> APlan:  # noqa: E501
-        """Make Plan for solving posed Problem using given Knowledge & Informational Resources."""
-
-    @abstractmethod
-    def update_plan_resources(self, plan: APlan, /, problem: str, resources: set[AResource],
-                              *, knowledge: set[Knowledge] | None = None) -> APlan:
-        """Make updated Plan for solving posed Problem using given Knowledge & relevant Informational Resources."""
+    def construct_program(self, problem: str, *,
+                          knowledge: set[Knowledge] | None = None,
+                          resources: set[AResource] | None = None) -> AProgram:
+        """Construct a Program for solving the posed Problem using given Knowledge & Resources."""
 
 
 AProgrammer: TypeVar = TypeVar('AProgrammer', bound=AbstractProgrammer, covariant=False, contravariant=False)
