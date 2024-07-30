85b91452d85935e42348e55eb11d9859c4e59193
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-07-30 09:50:07 -0700

    update openssa.l2.programming.hierarchical.planner.HTPlanner.construct_htp(...) method

--- openssa/l2/programming/hierarchical/planner.py
+++ openssa/l2/programming/hierarchical/planner.py
@@ -11,11 +11,12 @@ the complexity of which is controlled by 2 key parameters `max_depth` and `max_s
 
 from __future__ import annotations
 
-from dataclasses import dataclass
+from dataclasses import dataclass, replace
 from typing import Self, TYPE_CHECKING
 
 from openssa.l2.programming.abstract.programmer import AbstractProgrammer
 from openssa.l2.knowledge._prompts import knowledge_injection_lm_chat_msgs
+from openssa.l2.task import Task
 
 from .plan import HTP
 from ._prompts import HTP_PROMPT_TEMPLATE, HTP_WITH_RESOURCES_PROMPT_TEMPLATE
@@ -41,35 +42,41 @@ class HTPlanner(AbstractProgrammer):
                       knowledge: set[Knowledge] | None = None,
                       resources: set[AResource] | None = None) -> HTP:
         """Construct HTP for solving posed Problem with given Knowledge and Informational Resources."""
-        prompt: str = (
-            HTP_WITH_RESOURCES_PROMPT_TEMPLATE.format(problem=problem,
-                                                      resource_overviews={resource.unique_name: resource.overview
-                                                                          for resource in resources},
-                                                      max_depth=1,
-                                                      max_subtasks_per_decomp=self.max_subtasks_per_decomp)
-            if resources
-            else HTP_PROMPT_TEMPLATE.format(problem=problem,
-                                            max_depth=1,
-                                            max_subtasks_per_decomp=self.max_subtasks_per_decomp)
-        )
+        if self.max_depth > 0:
+            prompt: str = (
+                HTP_WITH_RESOURCES_PROMPT_TEMPLATE.format(problem=problem,
+                                                          resource_overviews={resource.unique_name: resource.overview
+                                                                              for resource in resources},
+                                                          max_depth=1,
+                                                          max_subtasks_per_decomp=self.max_subtasks_per_decomp)
+                if resources
+                else HTP_PROMPT_TEMPLATE.format(problem=problem,
+                                                max_depth=1,
+                                                max_subtasks_per_decomp=self.max_subtasks_per_decomp)
+            )
 
-        htp_dict: HTPDict = {}
-        knowledge_lm_hist: LMChatHist | None = (knowledge_injection_lm_chat_msgs(knowledge=knowledge)
-                                                if knowledge
-                                                else None)
-        while not (isinstance(htp_dict, dict) and htp_dict):
-            htp_dict: HTPDict = self.lm.get_response(prompt, history=knowledge_lm_hist, json_format=True)
+            htp_dict: HTPDict = {}
+            knowledge_lm_hist: LMChatHist | None = (knowledge_injection_lm_chat_msgs(knowledge=knowledge)
+                                                    if knowledge
+                                                    else None)
+            while not (isinstance(htp_dict, dict) and htp_dict):
+                htp_dict: HTPDict = self.lm.get_response(prompt, history=knowledge_lm_hist, json_format=True)
 
-        htp: HTP = HTP.from_dict(htp_dict)
+            htp: HTP = HTP.from_dict(htp_dict)
 
-        if resources:
-            htp.fix_missing_resources()
+            if resources:
+                htp.fix_missing_resources()
 
-        htp.task.ask: str = problem
-        htp.programmer: Self = self
-        htp.max_further_depth: int = self.max_depth - 1
+            htp.task.ask: str = problem
+            htp.programmer: Self = self
 
-        return htp
+            sub_htp_programmer: Self = replace(self, max_depth=self.max_depth - 1)
+            for sub_htp in htp.sub_htps:
+                sub_htp.programmer: Self = sub_htp_programmer
+
+            return htp
+
+        return HTP(task=Task(ask=problem, resources=resources))
 
     # alias
     construct_program = construct_htp
