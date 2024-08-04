438852353dbaa7f7370914ab6fc0606856927f04
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-08-03 18:02:15 -0700

    update examples/semiconductor Makefile targets

--- examples/semiconductor/make.bat
+++ examples/semiconductor/make.bat
@@ -5,7 +5,22 @@
 :: =======
 SET TARGET=%1
 
+IF "%TARGET%"=="agent-solve" GOTO agent-solve
+
+IF "%TARGET%"=="semikong-answer" GOTO semikong-answer
+
 IF "%TARGET%"=="streamlit-run" GOTO streamlit-run
+
+
+:: INFERENCING
+:: ===========
+:agent-solve
+  poetry run python htp_oodar_agent.py %2
+  GOTO end
+
+:semikong-answer
+  poetry run python semikong_lm.py %2
+  GOTO end
 
 
 :: STREAMLIT APP
