510bbddaa584d95003aac28c739b8cd6d9055837
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-08-03 20:53:33 -0700

    remove openssa.core.util.lm.test_hf_lm submodule

--- openssa/core/util/lm/test_hf_lm.py
+++
@@ -1,4 +0,0 @@
-from openssa.l2.util.lm.huggingface_lm import HuggingFaceLM
-from openssa.l2.util.lm.config import LMConfig
-
-lm = HuggingFaceLM(model=LMConfig.DEFAULT_HF_LLAMA_MODEL, api_base=LMConfig.LLAMA_API_URL)
