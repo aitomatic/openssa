35b5eae55a3a622ab0824a1050866e9a70045c1b
aran-nakayama <aran0204.ambro@icloud.com>
2024-10-01 11:35:43 +0900

    use vannna to make sql and pass to the dana agent through dbresource

--- examples/use-rdb-resource/main.py
+++ examples/use-rdb-resource/main.py
@@ -1,17 +1,44 @@
-from pathlib import Path
 from dotenv import load_dotenv
+import os
 from openssa import DANA, DbResource
+from vanna.openai import OpenAI_Chat
+from vanna.chromadb import ChromaDB_VectorStore
+import yaml
 
 load_dotenv()
 
-# いらなそう
-LOCAL_CACHE_DOCS_DIR_PATH: Path = Path(__file__).parent / '.data'
 
+def load_yaml(file_path):
+    with open(file_path, 'r') as file:
+        return yaml.safe_load(file)
+
+
+config = load_yaml('db_config.yaml')
+db_host = config['database']['mysql']['host']
+db_database = config['database']['mysql']['database']
+db_user = config['database']['mysql']['username']
+db_password = config['database']['mysql']['password']
+db_port = config['database']['mysql']['port']
+
+
+class MyVanna(ChromaDB_VectorStore, OpenAI_Chat):
+    def __init__(self, config=None):
+        ChromaDB_VectorStore.__init__(self, config=config)
+        OpenAI_Chat.__init__(self, config=config)
+
+
+openai_api_key = os.getenv('OPENAI_API_KEY')
+
+vn_openai = MyVanna(config={'model': 'gpt-4o', 'api_key': openai_api_key})
+vn_openai.connect_to_mysql(host=db_host, dbname=db_database, user=db_user, password=db_password, port=db_port)
+
+question = "Please tell me cheapest item from items table."
+query = vn_openai.generate_sql(question)
 
 def get_or_create_agent() -> DANA:
     return DANA(
         # TODO: For Argument Use SQL first and use prompt later
-        resources=[DbResource(config_path="db_config.yaml", query="SELECT * FROM items")]
+        resources=[DbResource(config_path="db_config.yaml", query=query)]
     )
 
 
@@ -27,11 +54,12 @@ if __name__ == '__main__':
     QUESTION = (
         # TODO:ここを変更（プロンプト）
         'Please tell me cheapest item from items table.'
-        'Please answer in Japanese.'
     )
     answer = solve(QUESTION)
 
-# TODO: 出力情報の追加 (作成して使用されたSQLなど)
+    # TODO: 出力情報の追加 (vanna aiで作成して使用されたSQLなど)
     print('--------------------------------')
     print(answer)
+    print('--------------------------------')
+    print(query)
     print('--------------------------------')
