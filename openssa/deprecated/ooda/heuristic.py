2c4c1dd509f9e5796798ca350b1dd6a464242551
Christopher Nguyen <ctn@aitomatic.com>
2024-10-04 23:54:11 +0900

    Remove deprecated/

--- openssa/deprecated/ooda/heuristic.py
+++
@@ -1,8 +0,0 @@
-class Heuristic:
-
-    # Base class for heuristics
-    def should_subtask(self, task, llm, history):
-        pass
-
-    def decompose_task(self, task, llm, history):
-        pass
--- openssa/deprecated/ooda_rag/resources/rag_resource.py
+++
@@ -1,8 +0,0 @@
-from llama_index.core.retrievers import BaseRetriever
-from llama_index.core.query_engine import BaseQueryEngine
-
-
-class RagResource:
-    def __init__(self, query_engine: BaseQueryEngine, retriever: BaseRetriever) -> None:
-        self.query_engine = query_engine
-        self.retriever = retriever
