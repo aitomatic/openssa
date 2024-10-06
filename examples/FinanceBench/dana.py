0b2a5ef02de8bca5c91d9f1fc696d289a4a5a146
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-10-06 11:39:38 -0700

    refactor examples/FinanceBench

--- examples/FinanceBench/dana.py
+++ examples/FinanceBench/dana.py
@@ -12,13 +12,13 @@ from util import QAFunc, enable_batch_qa_and_eval, log_qa_and_update_output_file
 
 
 @cache
-def get_main_lm(use_llama3: bool = False):
-    return (HuggingFaceLM if use_llama3 else OpenAILM).from_defaults()
+def get_main_lm(use_llama: bool = False):
+    return (HuggingFaceLM if use_llama else OpenAILM).from_defaults()
 
 
 @cache
-def get_or_create_expert_program_store(use_llama3: bool = False) -> ProgramStore:
-    program_store = ProgramStore(lm=get_main_lm(use_llama3=use_llama3))
+def get_or_create_expert_program_store(use_llama: bool = False) -> ProgramStore:
+    program_store = ProgramStore(lm=get_main_lm(use_llama=use_llama))
 
     for program_name, htp_dict in EXPERT_PROGRAMS.items():
         htp = HTP.from_dict(htp_dict)
@@ -30,16 +30,16 @@ def get_or_create_expert_program_store(use_llama3: bool = False) -> ProgramStore
 @cache
 def get_or_create_agent(doc_name: DocName, expert_knowledge: bool = False, expert_programs: bool = False,
                         max_depth=3, max_subtasks_per_decomp=6,
-                        use_llama3: bool = False,
+                        use_llama: bool = False,
                         llama_index_openai_lm_name: str = LMConfig.OPENAI_DEFAULT_MODEL) -> DANA:
     # pylint: disable=too-many-arguments
     return DANA(knowledge={EXPERT_KNOWLEDGE} if expert_knowledge else None,
 
-                program_store=(get_or_create_expert_program_store(use_llama3=use_llama3)
+                program_store=(get_or_create_expert_program_store(use_llama=use_llama)
                                if expert_programs
                                else ProgramStore()),
 
-                programmer=HTPlanner(lm=get_main_lm(use_llama3=use_llama3),
+                programmer=HTPlanner(lm=get_main_lm(use_llama=use_llama),
                                      max_depth=max_depth, max_subtasks_per_decomp=max_subtasks_per_decomp),
 
                 resources={FileResource(path=Doc(name=doc_name).dir_path,
@@ -83,34 +83,34 @@ def solve_with_knowledge_and_program_store(fb_id: FbId) -> Answer:
         adaptations_from_known_programs=get_or_create_adaptations(doc_name=DOC_NAMES_BY_FB_ID[fb_id]))
 
 
-@enable_batch_qa_and_eval(output_name='DANA-wLlama3')
-@log_qa_and_update_output_file(output_name='DANA-wLlama3')
-def solve_with_llama3(fb_id: FbId) -> Answer:
-    return get_or_create_agent(doc_name=DOC_NAMES_BY_FB_ID[fb_id], use_llama3=True).solve(
+@enable_batch_qa_and_eval(output_name='DANA-wLlama')
+@log_qa_and_update_output_file(output_name='DANA-wLlama')
+def solve_with_llama(fb_id: FbId) -> Answer:
+    return get_or_create_agent(doc_name=DOC_NAMES_BY_FB_ID[fb_id], use_llama=True).solve(
         problem=QS_BY_FB_ID[fb_id],
         adaptations_from_known_programs=get_or_create_adaptations(doc_name=DOC_NAMES_BY_FB_ID[fb_id]))
 
 
-@enable_batch_qa_and_eval(output_name='DANA-wKnowledge-wLlama3')
-@log_qa_and_update_output_file(output_name='DANA-wKnowledge-wLlama3')
-def solve_with_knowledge_with_llama3(fb_id: FbId) -> Answer:
-    return get_or_create_agent(doc_name=DOC_NAMES_BY_FB_ID[fb_id], expert_knowledge=True, use_llama3=True).solve(
+@enable_batch_qa_and_eval(output_name='DANA-wKnowledge-wLlama')
+@log_qa_and_update_output_file(output_name='DANA-wKnowledge-wLlama')
+def solve_with_knowledge_with_llama(fb_id: FbId) -> Answer:
+    return get_or_create_agent(doc_name=DOC_NAMES_BY_FB_ID[fb_id], expert_knowledge=True, use_llama=True).solve(
         problem=QS_BY_FB_ID[fb_id],
         adaptations_from_known_programs=get_or_create_adaptations(doc_name=DOC_NAMES_BY_FB_ID[fb_id]))
 
 
-@enable_batch_qa_and_eval(output_name='DANA-wProgStore-wLlama3')
-@log_qa_and_update_output_file(output_name='DANA-wProgStore-wLlama3')
-def solve_with_program_store_with_llama3(fb_id: FbId) -> Answer:
-    return get_or_create_agent(doc_name=DOC_NAMES_BY_FB_ID[fb_id], expert_programs=True, use_llama3=True).solve(
+@enable_batch_qa_and_eval(output_name='DANA-wProgStore-wLlama')
+@log_qa_and_update_output_file(output_name='DANA-wProgStore-wLlama')
+def solve_with_program_store_with_llama(fb_id: FbId) -> Answer:
+    return get_or_create_agent(doc_name=DOC_NAMES_BY_FB_ID[fb_id], expert_programs=True, use_llama=True).solve(
         problem=QS_BY_FB_ID[fb_id],
         adaptations_from_known_programs=get_or_create_adaptations(doc_name=DOC_NAMES_BY_FB_ID[fb_id]))
 
 
-@enable_batch_qa_and_eval(output_name='DANA-wKnowledge-wProgStore-wLlama3')
-@log_qa_and_update_output_file(output_name='DANA-wKnowledge-wProgStore-wLlama3')
-def solve_with_knowledge_and_program_store_with_llama3(fb_id: FbId) -> Answer:
-    return get_or_create_agent(DOC_NAMES_BY_FB_ID[fb_id], expert_knowledge=True, expert_programs=True, use_llama3=True).solve(  # noqa: E501
+@enable_batch_qa_and_eval(output_name='DANA-wKnowledge-wProgStore-wLlama')
+@log_qa_and_update_output_file(output_name='DANA-wKnowledge-wProgStore-wLlama')
+def solve_with_knowledge_and_program_store_with_llama(fb_id: FbId) -> Answer:
+    return get_or_create_agent(DOC_NAMES_BY_FB_ID[fb_id], expert_knowledge=True, expert_programs=True, use_llama=True).solve(  # noqa: E501
         problem=QS_BY_FB_ID[fb_id],
         adaptations_from_known_programs=get_or_create_adaptations(doc_name=DOC_NAMES_BY_FB_ID[fb_id]))
 
@@ -121,7 +121,7 @@ if __name__ == '__main__':
     arg_parser.add_argument('--from-id', action='store_true')
     arg_parser.add_argument('--knowledge', action='store_true')
     arg_parser.add_argument('--prog-store', action='store_true')
-    arg_parser.add_argument('--llama3', action='store_true')
+    arg_parser.add_argument('--llama', action='store_true')
     args = arg_parser.parse_args()
 
     match (args.knowledge, args.prog_store, args.llama3):
@@ -138,16 +138,16 @@ if __name__ == '__main__':
             solve_func: QAFunc = solve_with_knowledge_and_program_store
 
         case (False, False, True):
-            solve_func: QAFunc = solve_with_llama3
+            solve_func: QAFunc = solve_with_llama
 
         case (True, False, True):
-            solve_func: QAFunc = solve_with_knowledge_with_llama3
+            solve_func: QAFunc = solve_with_knowledge_with_llama
 
         case (False, True, True):
-            solve_func: QAFunc = solve_with_program_store_with_llama3
+            solve_func: QAFunc = solve_with_program_store_with_llama
 
         case (True, True, True):
-            solve_func: QAFunc = solve_with_knowledge_and_program_store_with_llama3
+            solve_func: QAFunc = solve_with_knowledge_and_program_store_with_llama
 
     if not (fb_id := args.fb_id).startswith(FB_ID_COL_NAME):
         fb_id: FbId = f'{FB_ID_COL_NAME}_{fb_id}'
