ed10725aae9e01222dd3f75515bc08c74829c177
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2024-10-07 00:59:18 -0700

    update examples/semiconductor-etching-yieldguard

--- examples/semiconductor-etching-yieldguard/streamlit-main.py
+++ examples/semiconductor-etching-yieldguard/streamlit-main.py
@@ -1,13 +1,21 @@
 from collections import defaultdict
+from pathlib import Path
 
+from pandas import read_csv
 from loguru import logger
-
 import streamlit as st
 
 from dana import get_or_create_dana
 
 
 TITLE: str = 'Proactive _YieldGuard_ for Plasma Etchers'
+
+DATA_FILE_PATH: Path = 'measurements.csv'
+
+MONITORING_PROBLEMS: dict[str, str] = {
+    'RF Power Fluctuation': 'identify any RF power fluctuation issue from the data, and recommend what to do',
+}
+
 
 st.set_page_config(page_title=TITLE,
                    page_icon=None,
@@ -20,36 +28,31 @@ st.title(body=TITLE, anchor=None, help=None)
 st.image('YieldGuard.png')
 
 
-st.write('__PROBLEM__:')
-
-if 'typed_problem' not in st.session_state:
-    st.session_state.typed_problem: str = ''
-
-st.session_state.typed_problem: str = st.text_area(label='Problem',
-                                                   value=st.session_state.typed_problem,
-                                                   height=3,
-                                                   max_chars=None,
-                                                   key=None,
-                                                   help='Problem',
-                                                   on_change=None, args=None, kwargs=None,
-                                                   placeholder='Problem',
-                                                   disabled=False,
-                                                   label_visibility='collapsed')
+st.dataframe(data=read_csv(DATA_FILE_PATH),
+             width=None, height=None,
+             use_container_width=False,
+             hide_index=True,
+             column_order=None,
+             column_config=None,
+             key=None,
+             on_select='ignore',
+             selection_mode='multi-row')
 
 
 if 'dana_solutions' not in st.session_state:
     st.session_state.dana_solutions: defaultdict[str, str] = defaultdict(str)
 
-if st.button(label='SOLVE',
-             on_click=None, args=None, kwargs=None,
-             type='primary',
-             disabled=False,
-             use_container_width=False):
-    with st.spinner(text='_SOLVING..._'):
-        logger.level('DEBUG')
+for monitored_issue, problem in MONITORING_PROBLEMS.items():
+    if st.button(label=f'ANALYZE: {monitored_issue}',
+                 on_click=None, args=None, kwargs=None,
+                 type='primary',
+                 disabled=False,
+                 use_container_width=False):
+        with st.spinner(text='_ANALYZING..._'):
+            logger.level('DEBUG')
 
-        st.session_state.dana_solutions[st.session_state.typed_problem]: str = \
-            get_or_create_dana(use_semikong_lm=False).solve(problem=st.session_state.typed_problem)
+            st.session_state.dana_solutions[monitored_issue]: str = \
+                get_or_create_dana(use_semikong_lm=False).solve(problem=problem)
 
-if (solution := st.session_state.dana_solutions[st.session_state.typed_problem]):
-    st.markdown(body=solution.replace('$', r'\$'))
+    if (solution := st.session_state.dana_solutions[monitored_issue]):
+        st.markdown(body=solution.replace('$', r'\$'))
