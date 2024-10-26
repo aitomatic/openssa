9bf2495c96ff256df417a0e9bc245f340dbb3622
An Phan <phanhongan@gmail.com>
2024-10-25 19:03:32 -0700

    fix: set default values for DB connection

--- openssa/core/resource/db.py
+++ openssa/core/resource/db.py
@@ -30,11 +30,11 @@ class MySQLDatabase:
         }
 
     def create_engine(self):
-        username = os.getenv('DB_USERNAME')
-        password = os.getenv('DB_PASSWORD')
-        host = os.getenv('DB_HOST')
+        username = os.getenv('DB_USERNAME', 'root')
+        password = os.getenv('DB_PASSWORD', '')
+        host = os.getenv('DB_HOST', 'localhost')
         port = os.getenv('DB_PORT', '3306')
-        database = os.getenv('DB_NAME')
+        database = os.getenv('DB_NAME', 'test')
         connection_string = f'mysql+pymysql://{username}:{password}@{host}:{port}/{database}'
         return create_engine(connection_string)
 
